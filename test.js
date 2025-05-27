b738eaaa8ffbaefe249e8314830802ef5774a276
Sudheer gupta <Sudheergupta0508@gmail.com>
2025-05-27 15:02:56 +0530

    updated test case

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-// const audioAddon = require('./lib/node-miniaudio.node');
-// console.log(audioAddon.playAudio('C:\\addons\\local\\node-miniaudio\\output.wav')); // This should play your audio file
-
-
+const audioAddon = require('./lib/node-miniaudio.node');
 const path = require('path');
-const audioAddon = require('./build/Release/node-miniaudio');
+const filePath = path.join(__dirname, "output.wav");
 
-const filePath = path.join(__dirname, "output.wav"); // Ensures correct path
-console.log(filePath);
-audioAddon.playAudio(filePath);
+
+audioAddon.playAudio(filePath, function (response) {
+    console.log("miniaudio: ", response);
+});
